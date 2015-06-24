4214d8d1ed5a23914651712df6ec94e5b94cfd72
unknown <23047@bbc.qld.edu.au>
2015-06-24 20:25:44 +1000

    did some stuff with spi. Don't even know how I did it However has fixed some issues with slave1 transfer

--- slaves/slaves.h
+++ slaves/slaves.h
@@ -23,6 +23,12 @@
 
 #define TIMEOUT 20000	// in uS
 
+#define SPI_SR_TXCTR		0x0000f000 //Mask isolating the TXCTR
+#define SPI_WAIT() \
+	while ((SPI0_SR & SPI_SR_TXCTR) != 0); \
+	while (!(SPI0_SR & SPI_SR_TCF)); \
+	SPI0_SR |= SPI_SR_TCF;
+
 enum SERIALSTATUS{
 	SERIAL_SUCCESS,
 	SERIAL_NOTREADY,
@@ -94,22 +100,45 @@ public:
 		pinMode(cs, OUTPUT);
 		digitalWriteFast(cs, HIGH);
 	}
-	// send command to spi device. Also receives a byte
-	uint8_t txrx(uint8_t command){
+	
+	// send command and then send data. Returns received byte
+	uint8_t txrx(uint8_t command, uint8_t out = 0){
 		uint8_t in;
 		SPI.beginTransaction(SPISettings(SPI_CLOCK, MSBFIRST, SPI_MODE0));
 		digitalWriteFast(cs, LOW);
-		delayMicroseconds(1);
-		SPI.transfer(command); // not really the command any more. Just to save space
-		delayMicroseconds(50);
-		in = SPI.transfer(0);
+
+		//delayMicroseconds(1);
+		SPI.transfer(command);
+		delayMicroseconds(1); // give slave some time to know what it's going to send back next
+		
+		in = SPI.transfer(out);
+
 		digitalWriteFast(cs, HIGH);
 		SPI.endTransaction();
+
+		// Serial.print('\t');
+		// Serial.println(in);
 		return in;
 	}
+
 	// send and receive 16 bit data
-	int16_t txrx16(int16_t command){
-		return txrx(highByte(command)) << 8 | txrx(lowByte(command));
+	uint16_t txrx16(int16_t command, uint8_t out1 = 0, uint8_t out2 = 0){
+		uint8_t in1, in2;
+		SPI.beginTransaction(SPISettings(SPI_CLOCK, MSBFIRST, SPI_MODE0));
+		digitalWriteFast(cs, LOW);
+
+		SPI.transfer(command);
+		delayMicroseconds(50);
+
+		in1 = SPI.transfer(out1);
+		delayMicroseconds(50);
+
+		in2 = SPI.transfer(out2);
+
+		digitalWriteFast(cs, HIGH);
+		SPI.endTransaction();
+
+		return (in2 << 8) | in1;
 	}
 	// receive 16 bit data
 	int16_t rx16(uint8_t command){
@@ -170,18 +199,25 @@ public:
 		return txrx(SLAVE2_COMMANDS::TSOP_STRENGTH);
 	}
 	uint8_t getTSOPAngleStrength(uint8_t &angle, uint8_t &strength){
+		uint8_t one, two, three;
+
 		SPI.beginTransaction(SPISettings(SPI_CLOCK, MSBFIRST, SPI_MODE0));
 		digitalWriteFast(cs, LOW);
+		delayMicroseconds(1);
 
-		Serial.print(SPI.transfer(SLAVE2_COMMANDS::TSOP_ANGLE_BYTE)); Serial.print('\t');
-		delayMicroseconds(1);
-		Serial.print(SPI.transfer(0)); Serial.print('\t');
-		delayMicroseconds(1);
-		Serial.print(SPI.transfer(SLAVE2_COMMANDS::TSOP_STRENGTH)); Serial.print('\t');
-		delayMicroseconds(1);
-		Serial.println(SPI.transfer(0));
+		one = SPI.transfer(SLAVE2_COMMANDS::TSOP_ANGLE_BYTE);
+		delayMicroseconds(50);
+		two = SPI.transfer(SLAVE2_COMMANDS::TSOP_STRENGTH);
+		delayMicroseconds(50);
+		three = SPI.transfer(0);
+
 		digitalWriteFast(cs, HIGH);
 		SPI.endTransaction();
+
+		Serial.print(one); Serial.print('\t');
+		Serial.print(two); Serial.print('\t');
+		Serial.println(three);
+		return three;
 	}
 };
 
@@ -189,28 +225,23 @@ class SLAVE3 : public SPISLAVE{
 public:
 	SLAVE3(uint8_t chip_select) : SPISLAVE(chip_select){};
 	uint8_t moveMotorA(int16_t velocity){
-		txrx(SLAVE3_COMMANDS::MOVE1);
-		txrx16(velocity);
+		txrx16(SLAVE3_COMMANDS::MOVE1, highByte(velocity), lowByte(velocity));
 		return 0;
 	}
 	uint8_t moveMotorB(int16_t velocity){
-		txrx(SLAVE3_COMMANDS::MOVE2);
-		txrx16(velocity);
+		txrx16(SLAVE3_COMMANDS::MOVE2, highByte(velocity), lowByte(velocity));
 		return 0;
 	}
 	uint8_t moveMotorC(int16_t velocity){
-		txrx(SLAVE3_COMMANDS::MOVE3);
-		txrx16(velocity);
+		txrx16(SLAVE3_COMMANDS::MOVE3, highByte(velocity), lowByte(velocity));
 		return 0;
 	}
 	uint8_t moveMotorD(int16_t velocity){
-		txrx(SLAVE3_COMMANDS::MOVE4);
-		txrx16(velocity);
+		txrx16(SLAVE3_COMMANDS::MOVE4, highByte(velocity), lowByte(velocity));
 		return 0;
 	}
 	uint8_t moveMotorE(int16_t velocity){
-		txrx(SLAVE3_COMMANDS::MOVE5);
-		txrx16(velocity);
+		txrx16(SLAVE3_COMMANDS::MOVE5, highByte(velocity), lowByte(velocity));
 		return 0;
 	}
 	void getCS1(uint8_t &cs){
@@ -241,27 +272,11 @@ public:
 		v = rx16(SLAVE3_COMMANDS::V4);
 	}
 	uint8_t moveRobot(uint8_t dir, uint8_t velocity, int8_t rotation_velocity){
-		// txrx(SLAVE3_COMMANDS::MOVE);
-		// //delayMicroseconds(1);
-		// txrx(highByte(dir));
-		// //delayMicroseconds(1);
-		// txrx(lowByte(dir));
-		// //delayMicroseconds(1);
-		// txrx(velocity);
-		// //delayMicroseconds(1);
-		// txrx(rotation_velocity);
-		// //delayMicroseconds(1);
-		// //Serial.println(dir);
 		SPI.beginTransaction(SPISettings(SPI_CLOCK, MSBFIRST, SPI_MODE0));
 		digitalWriteFast(cs, LOW);
 		SPI.transfer(SLAVE3_COMMANDS::MOVE);
-		delayMicroseconds(1);
 		SPI.transfer(dir);
-		delayMicroseconds(1);
-		SPI.transfer(0x10);
-		delayMicroseconds(1);
 		SPI.transfer(velocity);
-		delayMicroseconds(1);
 		SPI.transfer(rotation_velocity);
 		digitalWriteFast(cs, HIGH);
 		SPI.endTransaction();
